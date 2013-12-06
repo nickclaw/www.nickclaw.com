9d81b3c25acdca872e45a86fc0a6df250f47f34a
Nicholas Clawson <nickclaw@gmail.com>
2013-12-05 16:44:41 -0800

    Fixed build script

--- build.sh
+++ build.sh
@@ -4,13 +4,15 @@ mv ./build ./~build
 
 #create needed build directories
 mkdir -p -v ./build/static/{css,image,js}
+mkdir -p -v ./build/views/pages
 
 #compile scss files
 sass --scss --style compressed  ./src/static/css/noscript.scss ./build/static/css/noscript.css
 sass --scss --style compressed  ./src/static/css/main.scss ./build/static/css/main.css
 
-#copy .htaccess and index.html
-cp ./src/.htaccess ./src/index.html ./build/
+#copy some files
+cp -R ./src/.htaccess ./src/node_modules ./src/server.js ./src/package.json ./build/
+cp -R ./src/views/pages ./build/views/pages
 
 #concatenate all my js files to temporary file
 cat $( ls -S ./src/static/js/*.js ) > ./build/static/js/.script.js
@@ -34,37 +36,29 @@ cat $(ls -S ./src/static/js/external/*.js) ./build/static/js/..script.js > ./bui
 rm ./build/static/js/.script.js ./build/static/js/..script.js
 
 # make index.html link to compiled script
-sed -e "s/<!--COMPILE-->.*<!--ENDCOMPILE-->/<script src=\"\/static\/js\/script\.js\"><\/script>/g" -e "s/\.gif\.png/\.gif/g" ./build/index.html > ./build/.index.html
-rm ./build/index.html;
-mv ./build/.index.html ./build/index.html
-
+sed -e "s/<!--COMPILE-->.*<!--ENDCOMPILE-->/<script src=\"\/static\/js\/script\.js\"><\/script>/g" -e "s/\.gif\.png/\.gif/g" ./src/views/template.html > ./build/views/template.html
 
 # copy images over
 cp -R ./src/static/image ./build/static
 
-# convert and compress pngs
+# convert and compress images
 cd build
-for f in `find . -name "*.png"`
-do
-    convert $f -fuzz 10% -transparent none -strip $f
-done
 	
-#convert and compress jpgs
-for f in `find . -name "*.jpg"`
-do
-    convert $f -quality 60 -resize 66% -strip $f
-done
+	#pngs
+	for f in `find . -name "*.png"`
+	do
+	    convert $f -fuzz 10% -transparent none -strip $f
+	done
+		
+	#jpgs
+	for f in `find . -name "*.jpg"`
+	do
+	    convert $f -quality 60 -resize 66% -strip $f
+	done
 
-#remove illustrator files
-for f in `find . -name "*.ai"`
-do
-	rm $f
-done
-
+	#remove illustrator files
+	for f in `find . -name "*.ai"`
+	do
+		rm $f
+	done
 cd ..
-
-#gzip all css js and html
-for f in `find ./build -name "*.css" -o -name "*.html" -o -name "*.js"`
-do 
-	gzip -c -9 $f > $f"gz"
-done
