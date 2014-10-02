d319bf1495aed2234f882a98a2d386704345a717
Nicholas Clawson <nickclaw@gmail.com>
2014-10-02 13:43:49 -0700

    Started app/site layout

---
+++ src/script/services/site.js
@@ -0,0 +1,89 @@
+angular.module('app.services')
+    .constant('site', {
+        sections: [
+            {
+                name: 'About Me',
+                title: 'about',
+                path: 'about',
+
+                templateUrl: 'about/main.html',
+
+                articles: [
+                    {
+                        name: 'Me',
+                        path: 'me',
+                        templateUrl: 'about/me.html'
+                    },
+                    {
+                        name: 'Now',
+                        path: 'now',
+                        templateUrl: 'about/now.html'
+                    }
+                ]
+            },
+            {
+                name: 'Experience',
+                title: 'experience',
+                path: 'experience',
+
+                templateUrl: 'experience/main.html',
+
+                articles: [
+                    {
+                        name: 'Disney',
+                        path: 'disney',
+                        templateUrl: 'experience/disney.html'
+                    },
+                    {
+                        name: 'Biology',
+                        path: 'biology',
+                        templateUrl: 'experience/biology.html'
+                    },
+                    {
+                        name: 'Ecocar',
+                        path: 'ecocar',
+                        templateUrl: 'experience/biology.html'
+                    }
+                ]
+            },
+            {
+                name: 'Projects',
+                title: 'projects',
+                path: 'projects',
+
+                templateUrl: 'experience/main.html',
+
+                articles: [
+                    {
+                        name: 'qdb',
+                        path: 'qdb',
+                        templateUrl: 'projects/qdb.html'
+                    },
+                    {
+                        name: 'Drown The Ave',
+                        path: 'dta',
+                        templateUrl: 'projects/dta.html'
+                    },
+                    {
+                        name: 'Window Tiler',
+                        path: 'tiler',
+                        templateUrl: 'projects/tiler.html'
+                    },
+                    {
+                        name: 'Sockdraw',
+                        path: 'sockdraw',
+                        templateUrl: 'projects/sockdraw.html'
+                    }
+                ]
+            },
+            {
+                name: 'Contact',
+                title: 'contact',
+                path: 'contact',
+
+                templateUrl: 'contact/main.html',
+
+                articles: []
+            }
+        ]
+    });
